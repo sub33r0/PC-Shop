56c14d3e00eb00c6a2cf86dd7fce059d5172ff47
sub33r0 <z3rowords@gmail.com>
2024-03-22 00:13:15 +0200

    add auth service

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { LoginComponent } from './login/login.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistrationComponent,
+    LoginComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
